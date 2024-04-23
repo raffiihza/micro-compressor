c79c6f0add3460244379f3239f117190b7d1e46e
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2024-04-23 21:39:21 +0700

    Add LLM

---
+++ backend/llm.js
@@ -0,0 +1,23 @@
+import { Ai } from './vendor/@cloudflare/ai.js';
+
+export default {
+  async fetch(request, env) {
+    const tasks = [];
+    const ai = new Ai(env.AI);
+
+    const url = new URL(request.url);
+    const prompt = url.searchParams.get('prompt');
+
+    if (!prompt) {
+      return new Response("please fill prompt", {status: 400});
+    }
+
+    let simple = {
+      prompt: prompt
+    };
+    let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', simple);
+    tasks.push({ inputs: simple, response });
+
+    return Response.json(tasks);
+  }
+};
