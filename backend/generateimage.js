79145d5badb4d0af0d99e36feee4aa4511ce7e0e
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2024-04-23 12:54:42 +0000

    second commit

---
+++ backend/generateimage.js
@@ -0,0 +1,33 @@
+export default {
+  async fetch(request, env) {
+    const ai = new Ai(env.AI);
+
+    const url = new URL(request.url);
+    const prompt = url.searchParams.get('prompt');
+
+    if (!prompt) {
+      return new Response("Please fill prompt", {
+        status: 400,
+        headers: {
+          'content-type': 'text/plain'
+        }
+      });
+    }
+
+    const inputs = {
+      prompt: prompt
+    };
+
+    const response = await ai.run(
+      '@cf/stabilityai/stable-diffusion-xl-base-1.0',
+      inputs
+    );
+
+    return new Response(response, {
+      headers: {
+        'content-type': 'image/png',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  }
+};
