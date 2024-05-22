503de3fad3c5808932d081d6c89c108af0c1cf19
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2024-05-22 07:03:53 +0000

    test: ignore files

--- db.py
+++ db.py
@@ -1,4 +1,9 @@
-# Put your postgres database URIs here
+import os
 
-DB_URL = ""
-DB_URL_2 = ""
+# Put your postgres database URIs here or on environment variables
+
+DB_URL = "postgres://"
+DB_URL_2 = "postgres://"
+
+DATABASE_URL = os.environ.get('DB_URL', DB_URL) 
+DATABASE_URL_2 = os.environ.get('DB_URL_2', DB_URL_2)
